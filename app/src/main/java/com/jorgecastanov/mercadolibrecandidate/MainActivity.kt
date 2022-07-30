3d519ab510062de2526cb40b2dfffdb7d922682f
JorgeCastanoV <jorge.castanov@perficient.com>
2022-07-29 19:05:28 -0500

    init repository commit

---
+++ app/src/main/java/com/jorgecastanov/mercadolibrecandidate/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.jorgecastanov.mercadolibrecandidate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.jorgecastanov.mercadolibrecandidate.ui.theme.MercadoLibreCandidateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MercadoLibreCandidateTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MercadoLibreCandidateTheme {
+        Greeting("Android")
+    }
+}
